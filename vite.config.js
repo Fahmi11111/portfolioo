bc47106c3afbc607d3b503f379a9ab275b072d24
Fahmi11111 <fahmimuhammad409@gmail.com>
2025-07-01 18:26:31 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio-fahmi/", // ← ini sudah benar
+  base: "/", // Untuk Netlify, base harus "/"
   plugins: [react()],
 });
